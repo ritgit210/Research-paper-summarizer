254fdddabdbd8038f055ddcc11d097b0563fa191
Ritesh Gond <riteshgond661@gmail.com>
2025-01-31 16:54:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def fetch_papers(query, max_results):
     return papers
 
 def summarize_text(text):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
     summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
     return summary[0]['summary_text']
 
